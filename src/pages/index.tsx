d0965624fbdf856a0d190ecbe055ff32d57a9059
Kavya Rishi <maniac@Kavyas-MacBook-Pro.local>
2024-11-07 22:18:07 +0530

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+}
